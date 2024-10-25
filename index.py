b4d4cc0acedd93ee3f89817116a468649da0ade5
Samuel Neche <neche.sdc@gmail.com>
2024-10-25 09:39:03 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,57 @@
+# name = "ade" # data type is string
+# age = 30 # data type is integer
+# height = 1.89 # data type of float
+# is_student = "false" # boolean
+# print(name)
+# print(type(name))
+# print(age)
+# print(type(age))
+# print(height)
+# print(type(height))
+# print(is_student)
+# print(type(is_student))
+
+
+# addition operator
+"+"
+#subtraction
+"-"
+# division
+"/"
+# multiplication operator
+"*"
+#Exponetial operator
+"**"
+#Modulos
+"%"
+
+a = 50
+b = 6
+# sum = a+b
+# print(sum)
+
+# sub = a-b
+# print(sub)
+
+# exp = a**b
+# print(exp)
+
+# mod = a%b
+# print(mod)
+
+# fruits = ['apple', 'pineapple', 'banana']
+# for fruits in fruits:
+#     print(fruits)
+
+# for t in range(10):
+#     print(t)
+
+def greet(name):
+    print(name)
+
+greet("joy")
+greet("ade")
+greet("mary")
+
+# def calc_area(l,w):
+#     print("the area of a rectangle is ", )
